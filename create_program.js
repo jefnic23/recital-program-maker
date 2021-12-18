b93e9d8736687e59209e94206ed23749e3750820
Jeff <jefnic23@gmail.com>
2021-12-17 22:38:16 -0500

    html fixes

--- create_program.js
+++ create_program.js
@@ -1,6 +1,6 @@
 const title = document.getElementById("title");
 const subtitle = document.getElementById('subtitle');
-const performers = document.getElementById('performers');
+const performances = document.getElementById('performances');
 const title_input = document.getElementById('titleInput');
 const subtitle_input = document.getElementById('subtitleInput');
 const piece_input = document.getElementById("pieceInput");
@@ -13,45 +13,45 @@ title_input.addEventListener("input", () => {
 })
 
 subtitle_input.addEventListener("input", () => {
-    subtitle.innerHTML = subtitle_input.value.replace(/\\/g, "<br/>");
+    subtitle.innerHTML = subtitle_input.value.replace(/\r?\n/, "<br/>");
 })
 
 enter_performance.addEventListener("click", () => {
-    var pieces = piece_input.value.split(/\\/g) || 'Untitled';
-    var composers = composer_input.value.split(/\\/g) || "Anon.";
+    var pieces = piece_input.value.split(/\r?\n/) || 'Untitled';
+    var composers = composer_input.value.split(/\r?\n/) || "Anon.";
+    var div = document.createElement("div");
+    performances.appendChild(div);
+    div.className = 'performance';
+    div.setAttribute("onclick", "this.remove()");
     for (let i = 0; i < pieces.length; i++) {
         var filler = ' . ';
-        var piece = pieces[i] || 'Untitled';
-        var composer = composers[i] || "Anon.";
+        var piece = "<b>" + pieces[i].trim() + "</b>" || 'Untitled';
+        var composer = composers[i].trim() || "Anon.";
         var text = piece + filler + composer;
-        var text_width = getTextWidth(text, getCanvasFontSize(performers));
+        var p = document.createElement("p");
+        div.appendChild(p);
+        p.className = "test";
+        p.innerHTML = text;
+        var text_width = Math.ceil(p.clientWidth);
         while (text_width < 450) {
             filler += ' . ';
             text = piece + filler + composer;
-            text_width = getTextWidth(text, getCanvasFontSize(performers));
-            if (text_width >= 450) {
+            p.innerHTML = text;
+            text_width = Math.ceil(p.clientWidth);
+            if (text_width > 450) {
+                filler = filler.slice(0, -3);
+                text = piece + filler + composer;
+                p.innerHTML = text;
                 break;
             }
         }
-        performers.innerHTML = text + "<br/>";
+        p.className = "piece";
     }
+    var p = document.createElement("p");
+    div.appendChild(p);
+    p.className = "performer";
+    p.innerHTML = "<i>" + performer_input.value.trim() + "</i>";
+    piece_input.value = '';
+    composer_input.value = '';
+    performer_input.value = '';
 })
-
-function getTextWidth(text, font) {
-    const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-    const context = canvas.getContext("2d");
-    context.font = font;
-    const metrics = context.measureText(text);
-    return metrics.width;
-}
-  
-function getCssStyle(element, prop) {
-    return window.getComputedStyle(element, null).getPropertyValue(prop);
-}
-
-function getCanvasFontSize(el = document.body) {
-    const fontWeight = getCssStyle(el, 'font-weight') || 'normal';
-    const fontSize = getCssStyle(el, 'font-size') || '12px';
-    const fontFamily = getCssStyle(el, 'font-family') || 'Helvetica';
-    return `${fontWeight} ${fontSize} ${fontFamily}`;
-}
